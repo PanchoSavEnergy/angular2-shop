9a009ea5fed4e57721c23d7c8671719b8e472a1c
PanchoSavEnergy <fco.marin.ortiz@gmail.com>
2022-07-06 16:55:56 -0400

    Commit objetos de prueba
    Se han agregado objetos de prueba para que tenga un 1% más de realismo.
    Ahora sería ideal agregar fotos más acorde, y subirlo a Github Pages si es posible.

--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -2,149 +2,145 @@ export const DATA: any  = {
   'categories': [
     {
       'categori_id': 1,
-      'name': 'drinks'
+      'name': 'Domótica'
     },
     {
       'categori_id': 2,
-      'name': 'lunch'
+      'name': 'Software'
     },
     {
       'categori_id': 3,
-      'name': 'food'
+      'name': 'Gamer'
     },
     {
       'categori_id': 4,
-      'name': 'sea'
+      'name': 'Electrónica'
     }
   ],
   'products': [
     {
       'id': 1,
-      'name': 'Lorem',
-      'price': '60.000',
-      'available': true,
-      'best_seller': true,
-      'categories': [
-        1,
-        4
-      ],
-      'img': 'http://lorempixel.com/200/100/food/',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
-    },
-    {
-      'id': 2,
-      'name': 'ipsum',
-      'price': '20.000',
-      'available': false,
-      'best_seller': false,
-      'categories': [
-        4
-      ],
-      'img': 'http://lorempixel.com/200/100/food/',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
-    },
-    {
-      'id': 3,
-      'name': 'dolor',
+      'name': 'WiFi Smart Bulb',
       'price': '10.000',
       'available': true,
       'best_seller': true,
       'categories': [
-        4
+        1,
       ],
-      'img': 'http://lorempixel.com/200/100/food/',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+      'img': 'http://loremflickr.com/200/100/smartbulb/',
+      'description': 'Este artefacto puede utilizarse con la app Tuya y ser controlado a través de tu celular.'
+    },
+    {
+      'id': 2,
+      'name': 'Smart Wireless IP Camera',
+      'price': '30.000',
+      'available': true,
+      'best_seller': true,
+      'categories': [
+        1
+      ],
+      'img': 'http://loremflickr.com/200/100/smartcamera/',
+      'description': 'Esta cámara tiene un lente de 2mpx, y puede ser controlada a la distancia a través de la app Tuya.'
+    },
+    {
+      'id': 3,
+      'name': 'Gaming headset',
+      'price': '15.000',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        3
+      ],
+      'img': 'http://loremflickr.com/200/100/headset/',
+      'description': 'Estos audífonos de alta calidad pueden ser utilizados en diferentes dispositivos como PC, consola, celular.'
     },
     {
       'id': 4,
-      'name': 'sit',
+      'name': 'Official Raspberry Pi Power Supply',
       'price': '35.000',
       'available': false,
       'best_seller': false,
       'categories': [
-        1,
-        2
+        4
       ],
-      'img': 'http://lorempixel.com/200/100/food/',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+      'img': 'http://loremflickr.com/200/100/raspberrypi/',
+      'description': 'Este cargador oficial permite asegurar el óptimo funcionamiento de una Raspberry Pi 3.'
     },
     {
       'id': 5,
-      'name': 'amet',
-      'price': '12.000',
+      'name': 'Smart TV Hyunday',
+      'price': '170.000',
       'available': true,
-      'best_seller': true,
+      'best_seller': false,
       'categories': [
         1,
         4
       ],
-      'img': 'http://lorempixel.com/200/100/food/',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+      'img': 'http://loremflickr.com/200/100/smarttv/',
+      'description': 'Este televisor contiene varias aplicaciones como Netflix, Spotify y Youtube.'
     },
     {
       'id': 6,
-      'name': 'consectetur',
-      'price': '120.000',
+      'name': 'Notebook Hyunday',
+      'price': '180.000',
       'available': true,
       'best_seller': false,
       'categories': [
         1,
         4
       ],
-      'img': 'http://lorempixel.com/200/100/food/',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+      'img': 'http://loremflickr.com/200/100/laptop/',
+      'description': 'Este notebook sirve para uso moderado, ideal para trabajo de oficina.'
     },
     {
       'id': 7,
-      'name': 'adipiscing',
-      'price': '50.000',
+      'name': 'SanDisk addapter',
+      'price': '20.000',
       'available': false,
       'best_seller': false,
-      'categories': [
-        1,
-        3
-      ],
-      'img': 'http://lorempixel.com/200/100/food/',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
-    },
-    {
-      'id': 8,
-      'name': 'elit',
-      'price': '2000',
-      'available': true,
-      'best_seller': false,
-      'categories': [
-        1,
-        3
-      ],
-      'img': 'http://lorempixel.com/200/100/food/',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
-    },
-    {
-      'id': 9,
-      'name': 'Maecenas',
-      'price': '150.000',
-      'available': true,
-      'best_seller': true,
       'categories': [
         2,
         4
       ],
-      'img': 'http://lorempixel.com/200/100/food/',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+      'img': 'http://loremflickr.com/200/100/microsd/',
+      'description': 'Este adaptador permite conectar una tarjeta microSD a tu computador.'
+    },
+    {
+      'id': 8,
+      'name': 'Software Manager',
+      'price': '100.000',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        2
+      ],
+      'img': 'http://loremflickr.com/200/100/qt/',
+      'description': 'Este software permite monitorear a otro, entregando mensajes cuando ocurre algún error en el software monitoreado.'
+    },
+    {
+      'id': 9,
+      'name': 'Tótem de atención',
+      'price': '1.500.000',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        2,
+        4
+      ],
+      'img': 'http://loremflickr.com/200/100/clienttotem/',
+      'description': 'Este tótem suele utilizarse en el comercio, para atender a los clientes cuando entrar a nuestro negocio.'
     },
     {
       'id': 10,
-      'name': 'eu',
-      'price': '200.000',
-      'available': false,
+      'name': 'Consola Retro SNES',
+      'price': '100.000',
+      'available': true,
       'best_seller': true,
       'categories': [
-        2,
         3
       ],
-      'img': 'http://lorempixel.com/200/100/food/',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+      'img': 'http://loremflickr.com/200/100/snes/',
+      'description': 'Esta consola contiene horas de diversión, donde puedes jugar todos tus títulos favoritos de la consola SNES.'
     }
   ]
 };
